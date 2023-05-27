59ceb01835a1c0e6023d1ab837c273aa46138c63
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-26 23:26:55 -0600

    primera version

---
+++ Listas.py
@@ -0,0 +1,12 @@
+#Listas
+
+lista = ["Lunes","Martes","Miercoles","Jueves","Viernes",1,2,3.56,[76]]
+
+print(lista)
+
+#Sublista
+print(lista[1:4])
+#Imprimir desde el final
+print(lista[-1])
+
+#Carolina EM
